0a2e340daf09f7d334ed534007e2b67f302046fb
Aimini <2026084033@qq.com>
2019-10-12 04:38:53 +0800

    test: improve benchtest method - push test code in top_test.sv in to a task, now we can test a file by simply call this task. - add new code generate python script, now using in ori.py

--- src/testbench/top_test.sv
+++ src/testbench/top_test.sv
@@ -17,10 +17,8 @@ module top_test();
     logic[31:0] previous_dbg_arg;
 
     string dbg_target = "sw_dbg";
-    string target_name[] = '{dbg_target,"lui","ori"};
-    string test_target = target_name[1];
-    string test_file = {"asm/temp/", test_target, ".asm.hextext"};
-    string regchk_file = {"asm/temp/", test_target, ".reg.hextext"};
+    string target_name[] = '{dbg_target,"lui","ori","ori_2"};
+    // string test_target = target_name[1];
 
     always_comb begin
         reg_file = unit_top.unit_core.unit_decode.unit_rf.file;
@@ -33,8 +31,16 @@ module top_test();
         dbg_arg = unit_top.unit_memory.unit_debug_ram.datas;
     end
 
+    function automatic string get_test_filename(string target);
+        return {"asm/temp/", target, ".asm.hextext"};
+    endfunction
+
+    function automatic string get_regchk_filename(string target);
+        return  {"asm/temp/", target, ".reg.hextext"};
+    endfunction
+
     /* fill for sw_dbg test */
-    task fill_regsiter_sw_dbg();
+    function automatic void fill_regsiter_sw_dbg();
        unit_top.unit_core
        .unit_decode.unit_rf.file[2] = 32'hffff0000; //v0 = 0xFFFF0000
        for(int i = 0; i < 8; ++i)
@@ -42,83 +48,113 @@ module top_test();
             .unit_decode.unit_rf.file[16 + i] =  1 << i; //s0 - s7
         unit_top.unit_core
        .unit_decode.unit_rf.file[16] = 32'hffff0000; //s0 = 0xFFFF0000
-    endtask
+    endfunction
+
+    function automatic void check_sw_dbg_arg();
+        logic ret = 1;
+         for(int i = 0; i < 8; ++i) begin
+            $display("dbg(%0d) = %8h",i,dbg_arg[i]);
+        end
+        // check dbg_arg memory 
+        if(dbg_arg[1] == 32'h0000_0020) begin
+            assert (dbg_arg[0] === 32'hffff_0000)
+            else  $error("dbg(0) != 32'hffff_0000!");
+            for(int i = 1; i < 8; ++i) begin
+                assert (dbg_arg[i] === (1 << i))
+                else  begin
+                   $error("sw_dbg failed: dbg(%0d) != %8h",i,dbg_arg[i]);
+                   ret = 0; 
+                end
+            end
+        end
+        // return ret;
+    endfunction
     
     /** check reg file **/
     function automatic void check_regfile(string filename,logic[31:0]  rf[31:0],logic ignore_gp_sp);
         logic[31:0]  temp[31:0];
-        $readmemh(regchk_file, temp);
-            for(int i = 0; i < 32; ++i) begin
-                if(ignore_gp_sp & (i == 28 | i == 29))
-                    continue;
-                assert (temp[i] === rf[i]) 
-                else   $error("register file check failed at %0d: %8h != %8h ",i,temp[i],rf[i]);
-            end
-    endfunction;
+        $readmemh(filename, temp);
+        for(int i = 0; i < 32; ++i) begin
+            if(ignore_gp_sp & (i == 28 | i == 29))
+                continue;
+            assert (temp[i] === rf[i]) 
+            else  $error("register file check failed at %0d: %8h != %8h ",i,temp[i],rf[i]);
+        end
+    endfunction
 
 
-    initial begin
+    task automatic new_test(string target,logic fill_reg);
+        string test_filename =  get_test_filename(target);
+        string regchk_filename = get_regchk_filename(target);
+        logic exit = 0;
+
+        $readmemh(test_filename, unit_top.unit_memory.unit_ins_rom.im);
+        
         reset = 1;
-
-        $readmemh(test_file, unit_top.unit_memory.unit_ins_rom.im);
         @(negedge clk) begin
             reset = 1;
         end
         
         @(negedge clk) begin
-            if(test_target == dbg_target) begin
+            reset = 0;
+            if(fill_reg) begin
                 fill_regsiter_sw_dbg();
             end
-
-            reset = 0;
         end
-    end
 
+        while (~exit) begin
+            @(negedge clk) begin
+                if(previous_dbg_arg === dbg_arg[0])
+                    continue;
 
-    always @(negedge clk) begin
-        if(previous_dbg_arg !== dbg_arg[0])  begin
-            case(dbg_arg[0])
-                0: $finish;
+                case(dbg_arg[0])
+                    0: exit = 1;
 
-                1: begin
-                    assert(dbg_arg[1] !== dbg_arg[2])
-                    else $error("assert equal failed : %8h != %8h",dbg_arg[1],dbg_arg[2]);
-                end
-
-                2:  begin
-                    assert(dbg_arg[1] !== dbg_arg[2])
-                    else $error("assert not equal failed : %8h == %8h",dbg_arg[1],dbg_arg[2]);
-                end
-
-                // check regiter file with pattern
-                // 32'h0005_0000 32'h000A_0000 32'h0014_0000 32'h0028_0000
-                // 32'h0050_0000 32'h00A0_0000 32'h0140_0000 32'h0280_0000 and so on
-                32'h0001_0000: begin
-                    if(dbg_arg[1] === 32'h0001_0000)
-                        check_regfile(regchk_file,reg_file,1);
-                    else
-                        check_regfile(regchk_file,reg_file,0);
-                end
-
-                32'hffff_0000: begin
-                    for(int i = 0; i < 8; ++i) begin
-                        $display("dbg(%0d) = %8h",i,dbg_arg[i]);
+                    1: begin
+                        assert(dbg_arg[1] !== dbg_arg[2])
+                        else $error("assert equal failed : %8h != %8h",dbg_arg[1],dbg_arg[2]);
                     end
-                    // check dbg_arg memory 
-                    if(dbg_arg[1] == 32'h0000_0020) begin
-                        assert (dbg_arg[0] === 32'hffff_0000)
-                        else  $error("dbg(0) != 32'hffff_0000!");
-                        for(int i = 1; i < 8; ++i) begin
-                            assert (dbg_arg[i] === (1 << i))
-                            else  $error("sw_dbg failed: dbg(%0d) != %8h",i,dbg_arg[i]);
+
+                    2:  begin
+                        assert(dbg_arg[1] !== dbg_arg[2])
+                        else $error("assert not equal failed : %8h == %8h",dbg_arg[1],dbg_arg[2]);
+                    end
+
+                    
+                    32'h0001_0000: begin
+                        if(dbg_arg[1] === 32'h0001_0000) begin
+                             //ignore $gp, $sp
+                            check_regfile(regchk_filename, reg_file, 1);
+                        end  else begin   //check all
+                            check_regfile(regchk_filename, reg_file, 0);
                         end
                     end
-                end
 
-                default:
-                    $error("unsupport check!");
-            endcase
+                    32'hffff_0000: begin
+                        for(int i = 0; i < 8; ++i) begin
+                            $display("dbg(%0d) = %8h",i,dbg_arg[i]);
+                        end   // check dbg_arg memory 
+                        if(dbg_arg[1] == 32'h0000_0020) begin
+                            check_sw_dbg_arg();
+                        end
+                    end
+
+                    default:
+                        $error("unsupport check!");
+                endcase
+            end
+            previous_dbg_arg = dbg_arg[0];
         end
-        previous_dbg_arg = dbg_arg[0];
+    endtask
+
+    initial begin
+         // new_test(.target(target_name[0]),.fill_reg('1));
+         // new_test(.target(target_name[1]),.fill_reg(0));
+         new_test(.target(target_name[2]),.fill_reg(0));
+         new_test(.target(target_name[3]),.fill_reg(0));
+         $finish;
     end
+
+
+    
 endmodule
